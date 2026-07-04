7a390fc3ef5c5e8d79da1edd752ddfbed135e6c2
Shraddha Dhote <soniyadhote8@gmail.com>
2026-07-03 23:30:40 -0700

    forced save cors server js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,11 @@ connectCloudinary()
 // middlewares
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
-app.use(cors())
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'token']
+}))
 
 // api endpoints
 app.use('/api/user',userRouter)
