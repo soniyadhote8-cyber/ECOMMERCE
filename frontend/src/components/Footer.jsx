834aed37d5c9d5a9ffcc35602b535723678416ed
Shraddha Dhote <soniyadhote8@gmail.com>
2026-07-04 12:05:53 -0700

    updated coypright text & footer email

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -26,14 +26,14 @@ const Footer = () => {
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
                 <li>+1-212-456-7890</li>
-                <li>contact@foreveryou.com</li>
+                <li>Shraddha@gmail.com</li>
             </ul>
         </div>
       </div>
 
       <div>
         <hr />
-        <p className='py-5 text-sm text-center'>Copyright 2024@ forever.com - All Right Reserved.</p>
+        <p className='py-5 text-sm text-center'>Copyright 2024@ shraddha.com - All Right Reserved.</p>
       </div>
 
     </div>
